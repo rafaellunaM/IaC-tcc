7aa34094548e1f506c15225334a6cc44d1dfdf8f
rafaellunaM <rafael.luna.cavalcanti@gmail.com>
2025-05-14 22:52:51 -0300

    adjust providers.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,55 +20,6 @@ terraform {
   }
 }
 
-locals {
-  access_aws = chomp(regex("AWS_ACCESS_KEY_ID=(.*)", file("../config/aws.env"))[0])
-  secret_aws = chomp(regex("AWS_SECRET_ACCESS_KEY=(.*)", file("../config/aws.env"))[0])
-  region   = var.config[0].eks.region
-}
-
-provider "aws" {
-  region     = local.region
-  access_key = local.access_aws
-  secret_key = local.secret_aws
-}
-
-data "aws_eks_cluster" "eks_cluster" {
-  name = module.eks.eks_cluster_name
-
-  depends_on = [module.eks]
-}
-
-data "aws_eks_cluster_auth" "eks_cluster" {
-  name = module.eks.eks_cluster_name
-  depends_on = [module.eks]
-}
-
-data "aws_region" "current" {}
-data "aws_availability_zones" "available" {}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster.token
-}
-
-provider "kubectl" {
-  alias                  = "gavinbunney"
-  host                   = data.aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster.token
-  load_config_file       = false
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks_cluster.token
-  }
-}
-
-
 module "eks" {
   source = "./modules/eks"
   config = var.config
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,47 +1,47 @@
-# locals {
-#   access_aws = chomp(regex("AWS_ACCESS_KEY_ID=(.*)", file("../config/aws.env"))[0])
-#   secret_aws = chomp(regex("AWS_SECRET_ACCESS_KEY=(.*)", file("../config/aws.env"))[0])
-#   region   = var.config[0].eks.region
-# }
+locals {
+  access_aws = chomp(regex("AWS_ACCESS_KEY_ID=(.*)", file("../config/aws.env"))[0])
+  secret_aws = chomp(regex("AWS_SECRET_ACCESS_KEY=(.*)", file("../config/aws.env"))[0])
+  region   = var.config[0].eks.region
+}
 
-# provider "aws" {
-#   region     = local.region
-#   access_key = local.access_aws
-#   secret_key = local.secret_aws
-# }
+provider "aws" {
+  region     = local.region
+  access_key = local.access_aws
+  secret_key = local.secret_aws
+}
 
-# data "aws_eks_cluster" "eks_cluster" {
-#   name = module.eks.eks_cluster_name
+data "aws_eks_cluster" "eks_cluster" {
+  name = module.eks.eks_cluster_name
 
-#   depends_on = [module.eks]
-# }
+  depends_on = [module.eks]
+}
 
-# data "aws_eks_cluster_auth" "eks_cluster" {
-#   name = module.eks.eks_cluster_name
-#   depends_on = [module.eks]
-# }
+data "aws_eks_cluster_auth" "eks_cluster" {
+  name = module.eks.eks_cluster_name
+  depends_on = [module.eks]
+}
 
-# data "aws_region" "current" {}
-# data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+data "aws_availability_zones" "available" {}
 
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.eks_cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.eks_cluster.token
-# }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks_cluster.token
+}
 
-# provider "kubectl" {
-#   alias                  = "gavinbunney"
-#   host                   = data.aws_eks_cluster.eks_cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.eks_cluster.token
-#   load_config_file       = false
-# }
+provider "kubectl" {
+  alias                  = "gavinbunney"
+  host                   = data.aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks_cluster.token
+  load_config_file       = false
+}
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = data.aws_eks_cluster.eks_cluster.endpoint
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-#     token                  = data.aws_eks_cluster_auth.eks_cluster.token
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks_cluster.token
+  }
+}
